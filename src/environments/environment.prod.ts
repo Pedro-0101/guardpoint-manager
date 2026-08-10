10eacb6cb7c43cd471269f0ebaaba56857b0e7a9
Pedro-0101 <pedro.tiete@outlook.com>
2026-08-10 15:30:24 -0300

    chore(prod): apontar para guardpoint-server.railway.internal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1',
-  wsUrl: '/ws',
+  apiUrl: 'https://guardpoint-server.railway.internal/api/v1',
+  wsUrl: 'wss://guardpoint-server.railway.internal/ws',
 };
